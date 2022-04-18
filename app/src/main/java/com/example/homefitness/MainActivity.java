88e14bc4739fa3a10e56cecdab742e1222a35618
thang.pham2 <thang.pham2@ekoios.vn>
2022-04-18 23:00:04 +0700

    authentication

--- app/src/main/java/com/example/homefitness/MainActivity.java
+++ app/src/main/java/com/example/homefitness/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.homefitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=(Button) findViewById(R.id.button);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                startActivity(new Intent(this,LoginActivity.class) );
+                break;
+        }
+
     }
 }

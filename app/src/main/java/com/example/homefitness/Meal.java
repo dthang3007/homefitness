12fdb021b04b357512680aa8e1a68b9bf902f34c
Hong Duong <duonghongle0610@gmail.com>
2022-04-26 02:39:27 +0700

    final

--- app/src/main/java/com/example/homefitness/Meal.java
+++ app/src/main/java/com/example/homefitness/Meal.java
@@ -10,12 +10,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Meal extends AppCompatActivity implements View.OnClickListener {
     private Button Day1;
+
+    private Button profileButton;
+    private Button mealButton;
+    private Button exerciseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal);
         Day1 = (Button) findViewById(R.id.button2);
         Day1.setOnClickListener(this);
+
+        profileButton=findViewById(R.id.profile_btn);
+        mealButton=findViewById(R.id.meal_btn);
+        exerciseButton=findViewById(R.id.exercise_btn);
+        profileButton.setOnClickListener(this);
+        mealButton.setOnClickListener(this);
+        exerciseButton.setOnClickListener(this);
     }
 
     @Override
@@ -23,6 +35,19 @@ public class Meal extends AppCompatActivity implements View.OnClickListener {
         switch (view.getId()) {
             case R.id.button2:
                 startActivity(new Intent(this, Day1.class));
+                break;
+
+            case R.id.profile_btn:
+                Intent profileIntent = new Intent(Meal.this, ProfileActivity.class);
+                startActivity(profileIntent);
+                break;
+            case R.id.meal_btn:
+                Intent mealIntent = new Intent(Meal.this, Meal.class);
+                startActivity(mealIntent);
+                break;
+            case R.id.exercise_btn:
+                Intent exerciseIntent = new Intent(Meal.this, Exercise.class);
+                startActivity(exerciseIntent);
                 break;
         }
     }
